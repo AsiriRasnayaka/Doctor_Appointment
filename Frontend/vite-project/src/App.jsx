30f3d85dfd3ea733caac75831b027b780c574f4d
AsiriRasnayaka <asirirasnayaka123@gmail.com>
2026-02-13 21:11:40 +0530

    refactor routing structure and components in App.jsx and main.jsx

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Doctor from './pages/Doctor';
+import DoctorDetails from './pages/DoctorDetails';
+import MyAppointments from './pages/MyAppointments';
+import Profile from './pages/Profile';
+import Services from './pages/Services';
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/doctor" element={<Doctor />} />
+      <Route path="/doctor-details/:id" element={<DoctorDetails />} />
+      <Route path="/my-appointments" element={<MyAppointments />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/services" element={<Services />} />
+    </Routes>
+    </div>
+    
   )
 }
 
