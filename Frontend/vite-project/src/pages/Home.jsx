d492f891e1a45a14141c9238e3ecf4e01d6d6ad2
AsiriRasnayaka <asirirasnayaka123@gmail.com>
2026-02-20 11:10:11 +0530

    refactor Home component to include all sections for improved layout

--- Frontend/vite-project/src/pages/Home.jsx
+++ Frontend/vite-project/src/pages/Home.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import HealthSpecialities from '../components/HealthSpecialities'
+import Mission from '../components/Mission'
+import Featured from '../components/Featured'
+import HowItWorks from '../components/HowItWorks'
+import WhyChooseUs from '../components/WhyChooseUs'
+import OurDoctors from '../components/OurDoctors'
+import Testimonial from '../components/Testimonial'
+import Gallery from '../components/Gallery'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Hero />
+      <HealthSpecialities />
+      <Mission />
+      <Featured />
+      <HowItWorks />
+      <WhyChooseUs />
+      <OurDoctors />
+      <Testimonial />
+      <Gallery />
+    </div>
   )
 }
 
