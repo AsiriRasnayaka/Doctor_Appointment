5cb98ae0dfe23f94b17748ec1e35866ee05cc2d9
AsiriRasnayaka <asirirasnayaka123@gmail.com>
2026-04-21 10:58:01 +0530

    add user profile and appointment management to Navbar with logout functionality

--- Frontend/vite-project/src/components/Navbar.jsx
+++ Frontend/vite-project/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { AppContext } from '../context/AppContext';
 import { Link } from 'react-router-dom';
 import { assets} from '../assets/assets.js';
 import { X, Menu, User } from 'lucide-react';
-
+import toast from "react-hot-toast";
 
 const Navbar = () => {
     const { navigate, user, setUser } = useContext(AppContext);
@@ -17,6 +17,12 @@ const Navbar = () => {
         { name: "Services", link: "/services" },
         { name: "Doctor", link: "/doctor" },
     ];
+const handleLogout = () => {
+    toast.success("logout successful.");
+    setUser(null);
+    navigate("/");
+  };
+
   return (
     <div className="max-w-7xl mx-auto mt-8">
         {/* Desktop Menu */}
@@ -32,11 +38,51 @@ const Navbar = () => {
         </Link>
     ))}
     </div>
-    <button
-    onClick={() => navigate("/login")}
-    className="bg-secondary text-white cursor-pointer py-2 px-8 hover:bg-primary duration-300 transition-all rounded-lg">
-    Login
-    </button>
+    <div>
+          {user ? (
+            <div className="relative group">
+              <button className="p-2 rounded-full bg-gray-200 hover:bg-gray-300 transition">
+                <User className="w-6 h-6 text-gray-700" />
+              </button>
+              <div
+                className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg 
+                   opacity-0 scale-95 
+                   group-hover:opacity-100 group-hover:scale-100 
+                   transition-all duration-300 origin-top 
+                   z-50 inline-block"
+              >
+                <ul className="flex flex-col p-2 text-gray-700">
+                  <li
+                    onClick={() => navigate("/profile")}
+                    className="px-4 py-2 hover:bg-gray-100 rounded cursor-pointer"
+                  >
+                    Profile
+                  </li>
+
+                  <li
+                    onClick={() => navigate("/my-appointments")}
+                    className="px-4 py-2 hover:bg-gray-100 rounded cursor-pointer"
+                  >
+                    My Appointments
+                  </li>
+                  <li
+                    onClick={handleLogout}
+                    className="px-4 py-2 hover:bg-gray-100 rounded cursor-pointer text-red-500"
+                  >
+                    Logout
+                  </li>
+                </ul>
+              </div>
+            </div>
+          ) : (
+            <button
+              onClick={() => navigate("/login")}
+              className="bg-secondary text-white cursor-pointer py-2 px-8 hover:bg-primary duration-300 transition-all rounded-lg"
+            >
+              Login
+            </button>
+          )}
+        </div> 
     </div>
 
     {/* Mobile Navbar */}
@@ -67,12 +113,49 @@ const Navbar = () => {
                 {menu.name}
               </Link>
             ))}
-            <button
+            {user ? (
+              <div className="relative group">
+                <button className="p-2 rounded-full bg-gray-200 hover:bg-gray-300 transition">
+                  <User className="w-6 h-6 text-gray-700" />
+                </button>
+                <div
+                  className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg 
+                   opacity-0 scale-95 
+                   group-hover:opacity-100 group-hover:scale-100 
+                   transition-all duration-300 origin-top 
+                   z-50 inline-block"
+                >
+                  <ul className="flex flex-col p-2 text-gray-700">
+                    <li
+                      onClick={() => navigate("/profile")}
+                      className="px-4 py-2 hover:bg-gray-100 rounded cursor-pointer"
+                    >
+                      Profile
+                    </li>
+
+                    <li
+                      onClick={() => navigate("/my-appointments")}
+                      className="px-4 py-2 hover:bg-gray-100 rounded cursor-pointer"
+                    >
+                      My Appointments
+                    </li>
+                    <li
+                      onClick={handleLogout}
+                      className="px-4 py-2 hover:bg-gray-100 rounded cursor-pointer text-red-500"
+                    >
+                      Logout
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            ) : (
+              <button
                 onClick={() => navigate("/login")}
                 className="bg-secondary text-white cursor-pointer py-2 px-8 hover:bg-primary duration-300 transition-all rounded-lg"
               >
                 Login
               </button>
+            )}
         </div>
     )}
     </div>
